a09da695d684ad74435bdc5d6b01b6ec2bdd8d15
devWRM <mcwen@comcast.net>
2020-06-19 14:16:23 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,19 @@
+# Run with:       learn spec/02_shoe_spec.rb
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+    BRANDS << brand unless BRANDS.include?(brand)
+    # Below does not work because comparing brands array to single brand
+    #  if BRANDS != brand
+    #   BRANDS << brand
+    # end
   end
 
   def cobble
